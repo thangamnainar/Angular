061329b74a978996040444d4b8c7bfe36840e229
Thangam N <thangam.nainar0507@gmail.com>
2023-05-19 19:07:31 +0530

    saved

--- firstproject/src/app/app.component.ts
+++ firstproject/src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'My Firstproject';
+
+  a:string="StringInterPolation";
+  b:string="PropertyBinding";
+  c:string="EventBinding";
 }
